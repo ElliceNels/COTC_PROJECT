51978e141fb98606cddd0b5da757b8edb05c2ddb
ElliceNels <23371323@studentmail.ul.ie>
2025-03-02 13:50:52 +0000

    [Fix] Create log file doesnt exist

--- src/logger.py
+++ src/logger.py
@@ -2,11 +2,16 @@
 
 import logging
 from config import config
+import os
 
 logger = logging.getLogger(__name__)
 
 def setup_logger():
     """Setup the logger."""
+    # Check if the logging file path exists and create it if it doesn't exist
+    log_dir = os.path.dirname(config.logging.file_path)
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
 
     logging.basicConfig(
         level=config.logging.level, 
